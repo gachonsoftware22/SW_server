ce409684f2888d97e21849834fe849171f239383
박수한 <victor1919@naver.com>
2026-05-26 00:54:27 +0900

    cors 오리진

--- src/main/java/com/sw/sw_ai_doc/global/config/SecurityConfig.java
+++ src/main/java/com/sw/sw_ai_doc/global/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "https://d158biyr86tb19.cloudfront.net",
-                "https://ai-doctor-frontend-team.vercel.app"
+                "https://ai-doctor-frontend-team-t58j.vercel.app"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
